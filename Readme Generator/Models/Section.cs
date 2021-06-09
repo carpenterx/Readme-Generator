5e01bf9c94120fc503e58552309858f60b0a3b28
Sorin <jordache_sor@yahoo.com>
2021-06-09 16:53:28 +0300

    added section class
    fixes #9

---
+++ Readme Generator/Models/Section.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Readme_Generator.Models
+{
+    class Section
+    {
+        public string Name { get; set; }
+        public string Body { get; set; }
+    }
+}
