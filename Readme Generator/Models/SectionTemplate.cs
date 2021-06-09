d72c4166f8e75e1a8bea9ac50bf74f4a2a557e3d
Sorin <jordache_sor@yahoo.com>
2021-06-09 22:30:24 +0300

    section body now preserves whitespace

--- Readme Generator/Models/SectionTemplate.cs
+++ Readme Generator/Models/SectionTemplate.cs
@@ -1,8 +1,12 @@
-﻿namespace Readme_Generator.Models
+﻿using YamlDotNet.Core;
+using YamlDotNet.Serialization;
+
+namespace Readme_Generator.Models
 {
     public class SectionTemplate
     {
         public string Name { get; set; }
+        [YamlMember(ScalarStyle = ScalarStyle.Literal)]
         public string Body { get; set; }
     }
 }
