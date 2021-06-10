300c80ce6abc6f94db2b582d4c8851c35c92df64
Sorin <jordache_sor@yahoo.com>
2021-06-10 16:54:03 +0300

    fixed binding and notify property changed

--- Readme Generator/Models/SectionTemplate.cs
+++ Readme Generator/Models/SectionTemplate.cs
@@ -1,13 +1,41 @@
-﻿using YamlDotNet.Core;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using YamlDotNet.Core;
 using YamlDotNet.Serialization;
 
 namespace Readme_Generator.Models
 {
-    public class SectionTemplate
+    public class SectionTemplate : INotifyPropertyChanged
     {
         public string Name { get; set; }
         [YamlMember(ScalarStyle = ScalarStyle.Literal)]
-        public string Body { get; set; }
+        //public string Body { get; set; }
+        private string _body;
+        public string Body
+        {
+            get => _body;
+            set => SetField(ref _body, value);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return false;
+            }
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
 
         public SectionTemplate(SectionTemplate section)
         {
