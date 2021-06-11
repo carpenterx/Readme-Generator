5f353165ac2c55ab695ac0102f74d451b89820b3
Sorin <jordache_sor@yahoo.com>
2021-06-11 17:46:58 +0300

    added readmes window
    fixes #33

---
+++ Readme Generator/Windows/ReadmeWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Readme_Generator.Windows
+{
+    /// <summary>
+    /// Interaction logic for ReadmeWindow.xaml
+    /// </summary>
+    public partial class ReadmeWindow : Window
+    {
+        public ReadmeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
