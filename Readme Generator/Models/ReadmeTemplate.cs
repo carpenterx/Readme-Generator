81c8a4eb945c8efc5223de803782e732c14a7ee3
Sorin <jordache_sor@yahoo.com>
2021-06-11 17:15:47 +0300

    added readme template class
    fixes #18

---
+++ Readme Generator/Models/ReadmeTemplate.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Readme_Generator.Models
+{
+    class ReadmeTemplate
+    {
+        public string Name { get; set; }
+        public ObservableCollection<SectionTemplate> Sections { get; set; }
+    }
+}
