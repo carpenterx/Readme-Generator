6c8eda2e821a9b20cc8835d6f56058ade820f895
Sorin <jordache_sor@yahoo.com>
2021-06-12 13:39:51 +0300

    added icon to show current section

---
+++ Readme Generator/Models/Converters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Readme_Generator.Models
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isSelected = (bool)value;
+            if (isSelected)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Hidden;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
